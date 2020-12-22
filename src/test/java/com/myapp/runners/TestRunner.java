8510455da52b50adea4f8e10f4febc9d0c25ba9a
batireedui055 <batireedui055@gmail.com>
2020-12-22 13:00:48 -0500

    commetn

--- src/test/java/com/myapp/runners/TestRunner.java
+++ src/test/java/com/myapp/runners/TestRunner.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/myapp/step_definitions",
         dryRun = false
+//comment
 )
 public class TestRunner {
 }
